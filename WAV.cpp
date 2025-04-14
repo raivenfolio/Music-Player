6f52d6762198df4670f1747167420eeb8f374f25
Meg Raiven <raivenfolio@gmail.com>
2025-04-14 22:26:11 +0800

    updated ver

--- WAV.cpp
+++ WAV.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <math.h>
 using namespace std;
 
 //Riff chnk
@@ -22,18 +23,60 @@ const int bits_per_sample = 16;
 const string subchunk2_id = "data";
 const string subchunk2_size =  "----"; 
 
+const int duration = 2;
+const int max_amp = 32760;
+const double freq = 350;
+
 void write_as_bytes(ofstream &file, int value, int byte_size){
     file.write(reinterpret_cast<const char*>(&value), byte_size);
 }
-
-
-
-
 int main()
 {
 
     ofstream wavFile;
     wavFile.open("test.wav", ios::binary);
+
+    if(wavFile.is_open()){
+
+        //riff
+        wavFile << chunk_id;
+        wavFile << chunk_size;
+        wavFile << chunk_format;
+
+        //for the fmt
+        wavFile << subchunk1_id;
+        write_as_bytes(wavFile, subchunk1_size, 4);//turnign them into bytes to be written and read
+        write_as_bytes(wavFile, audio_format, 2);
+        write_as_bytes(wavFile, sampRate, 4);
+        write_as_bytes(wavFile, byteRate, 4);
+        write_as_bytes(wavFile, blockAlign, 2);
+        write_as_bytes(wavFile, bits_per_sample, 2);
+
+        //"data " sub chunk
+       wavFile << subchunk2_id;
+       wavFile << subchunk2_size;
+
+       int start_audio = wavFile.tellp();
+
+       for(int i=0; i <sampRate * duration; i++){
+            double amp = (double)i / sampRate * max_amp;
+            double value = sin((2 * 3.14 * i * freq) / sampRate);
+
+            double channel1 = amp * value / 2;
+            double channel2 = (max_amp - amp) * value;
+
+        write_as_bytes(wavFile, channel1, 2);
+        write_as_bytes(wavFile, channel2, 2); 
+       }
+
+       int end_audio = wavFile.tellp();
+       wavFile.seekp(start_audio - 4);
+        write_as_bytes(wavFile, end_audio - start_audio, 4);
+
+        wavFile.seekp(4, ios::beg); //beginning (beg) seeking 4 places after
+        write_as_bytes(wavFile, end_audio - 8, 4);
+    }
+    wavFile.close();
 
     return 0;
 }
