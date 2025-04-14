327435d367c238d44c62b3e66a608d80ebeba83d
Meg Raiven <raivenfolio@gmail.com>
2025-04-14 21:19:46 +0800

    update

--- WAV.cpp
+++ WAV.cpp
@@ -1,4 +1,33 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+//Riff chnk
+const string chunk_id = "RIFF";
+const string chunk_size = "----"; // four dahses because it takes 4 bytes maximum
+const string chunk_format = "WAVE"; //file type header
+
+//sub chunk
+const string subchunk1_id = "fmt";
+const int subchunk1_size = 16; //also takes maximum 16 bytes
+const int audio_format = 1; // pcm is equals to 1
+const int numChannels = 2; // 1 = mono, 2 = stereo
+const int sampRate = 44100;
+const int byteRate = sampRate * numChannels * (subchunk1_size/8);
+const int blockAlign = numChannels * (subchunk1_size/8);
+const int bits_per_sample = 16;
+
+//"data" sub chunk : The "data" subchunk contains the size of the data and the actual sound
+
+const string subchunk2_id = "data";
+const string subchunk2_size =  "----"; 
+
+
+
+
 int main()
 {
+
+
     return 0;
 }
